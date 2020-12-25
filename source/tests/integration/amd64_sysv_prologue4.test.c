917305f3c2f896947c26586513f7872c0806eb08
J. Protopopovs <jprotopopov1122@gmail.com>
2020-12-25 12:18:52 +0200

    Implement more AMD64 System-V tests

---
+++ source/tests/integration/amd64_sysv_prologue4.test.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "kefir/ir/function.h"
+#include "kefir/core/mem.h"
+#include "kefir/core/util.h"
+#include "kefir/codegen/amd64-sysv.h"
+#include "kefir/codegen/amd64/system-v/abi.h"
+
+kefir_result_t kefir_int_test(struct kefir_mem *mem) {
+    struct kefir_codegen_amd64 codegen;
+    kefir_irfunction_t func;
+    struct kefir_amd64_sysv_function sysv_func;
+    REQUIRE_OK(kefir_codegen_amd64_sysv_init(&codegen, stdout, mem));
+    codegen.asmgen.settings.enable_comments = false;
+    codegen.asmgen.settings.enable_identation = false;
+
+    REQUIRE_OK(kefir_irfunction_alloc(mem, "func1", 17, 1, 0, &func));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_STRUCT, 0, 3));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_INT32, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_ARRAY, 0, 8));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_CHAR, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_FLOAT32, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_FLOAT64, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_UNION, 0, 2));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_ARRAY, 0, 2));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_FLOAT32, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_FLOAT64, 0, 0));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_STRUCT, 0, 1));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_ARRAY, 0, 20));
+    REQUIRE_OK(kefir_ir_type_append_v(&func.declaration.params, KEFIR_IR_TYPE_CHAR, 0, 0));
+
+    REQUIRE_OK(kefir_amd64_sysv_function_alloc(mem, &func, &sysv_func));
+    REQUIRE_OK(kefir_amd64_sysv_function_prologue(&codegen, &sysv_func));
+    REQUIRE_OK(kefir_amd64_sysv_function_free(mem, &sysv_func));
+
+    REQUIRE_OK(KEFIR_CODEGEN_CLOSE(&codegen.iface));
+    REQUIRE_OK(kefir_irfunction_free(mem, &func));
+    return KEFIR_OK;
+}
