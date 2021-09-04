e8e35fa8a51e69774de45178ec5d06ec7ebad366
J. Protopopovs <jprotopopov1122@gmail.com>
2021-09-04 22:48:20 +0300

    Fix result code categorization

--- source/core/basic-types.c
+++ source/core/basic-types.c
@@ -14,19 +14,25 @@ kefir_result_category_t kefir_result_get_category(kefir_result_t res) {
 
         case KEFIR_UNKNOWN_ERROR:
         case KEFIR_INTERNAL_ERROR:
+        case KEFIR_OS_ERROR:
+        case KEFIR_INVALID_PARAMETER:
+        case KEFIR_INVALID_CHANGE:
+        case KEFIR_INVALID_REQUEST:
+        case KEFIR_INVALID_STATE:
         case KEFIR_OUT_OF_BOUNDS:
+        case KEFIR_OBJALLOC_FAILURE:
         case KEFIR_MEMALLOC_FAILURE:
         case KEFIR_NOT_SUPPORTED:
         case KEFIR_NOT_IMPLEMENTED:
         case KEFIR_OUT_OF_SPACE:
             return KEFIR_RESULT_CATEGORY_GENERIC_ERROR;
 
+        case KEFIR_LEXER_ERROR:
         case KEFIR_SYNTAX_ERROR:
+        case KEFIR_ANALYSIS_ERROR:
         case KEFIR_NOT_CONSTANT:
         case KEFIR_STATIC_ASSERT:
             return KEFIR_RESULT_CATEGORY_LANGUAGE_ERROR;
-
-        default:
-            return KEFIR_RESULT_CATEGORY_UNKNOWN;
     }
+    return KEFIR_RESULT_CATEGORY_UNKNOWN;
 }
