48ffd821472dd8c46bfeeac2f1c5256edf817007
J. Protopopovs <jprotopopov1122@gmail.com>
2021-11-29 20:24:10 +0100

    Fix pointer subtraction issue

--- source/ast-translator/nodes/binary_operation.c
+++ source/ast-translator/nodes/binary_operation.c
@@ -149,7 +149,7 @@ static kefir_result_t translate_subtraction(struct kefir_mem *mem, struct kefir_
 
         REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IROPCODE_ISUB, 0));
         REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDU64(builder, KEFIR_IROPCODE_PUSHU64, type_info.size));
-        REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IROPCODE_UDIV, 0));
+        REQUIRE_OK(KEFIR_IRBUILDER_BLOCK_APPENDI64(builder, KEFIR_IROPCODE_IDIV, 0));
     } else {
         const struct kefir_ast_translator_resolved_type *cached_type = NULL;
         REQUIRE_OK(KEFIR_AST_TRANSLATOR_TYPE_RESOLVER_BUILD_OBJECT(
