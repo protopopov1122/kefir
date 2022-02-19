6b2ff208e19e1f120f3821b657ced2b1376c6548
J. Protopopovs <jprotopopov1122@gmail.com>
2022-02-19 17:09:22 +0100

    Increase maximum identifier length

--- source/lexer/tokens/identifier.c
+++ source/lexer/tokens/identifier.c
@@ -24,7 +24,7 @@
 #include "kefir/util/char32.h"
 #include "kefir/core/source_error.h"
 
-#define MAX_IDENTIFIER_LENGTH 64
+#define MAX_IDENTIFIER_LENGTH 255
 
 static kefir_result_t scan_identifier_nondigit(struct kefir_lexer_source_cursor *cursor, kefir_char32_t *target) {
     kefir_char32_t chr = kefir_lexer_source_cursor_at(cursor, 0);
