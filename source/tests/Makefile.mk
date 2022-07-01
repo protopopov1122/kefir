f0cdd013aa48fd1a2a5c2ae677b9fa6b4ca38639
J. Protopopovs <jprotopopov1122@gmail.com>
2022-07-01 10:37:40 +0200

    Update test runner

--- source/tests/Makefile.mk
+++ source/tests/Makefile.mk
@@ -1,9 +1,13 @@
-VALGRIND_OPTIONS=-q --trace-children=yes --track-origins=yes --expensive-definedness-checks=yes --leak-check=full --error-exitcode=127
+VALGRIND_OPTIONS=-q --trace-children=yes --track-origins=yes --leak-check=full --error-exitcode=127
 CC_TEST_FLAGS=-no-pie
 
 ifeq ($(PLATFORM),freebsd)
 VALGRIND_OPTIONS+=--suppressions=$(ROOT)/suppressions/freebsd.valgrind
 CC_TEST_FLAGS=
+endif
+
+ifneq ($(PLATFORM),openbsd)
+VALGRIND_OPTIONS+=--expensive-definedness-checks=yes
 endif
 
 include source/tests/unit/Makefile.mk
